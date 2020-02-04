e05b9a1415347427d8539a1d6c0a7697300ccea2
Aman Dhiman <amandhiman987@gmail.com>
2020-02-04 18:25:35 +0530

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,23 @@
+(function ($) {
+    $('.home-banner').mousemove(function(e){
+
+        var wx = $(window).width();
+        var wy = $(window).height();
+
+        var x = e.pageX - this.offsetLeft;
+        var y = e.pageY - this.offsetTop;
+
+        var newx = x - wx/2;
+        var newy = y - wy/2;
+
+        $('.home-banner .slide-item .moving-content').each(function(){
+            var speed = $(this).attr('data-speed');
+            if($(this).attr('data-revert')) speed *= -1;
+            TweenMax.to($(this), 1, {x: (1 - newx*speed), y: (1 - newy*speed)});
+        });
+
+    });
+
+    $('.datepicker input').datepicker({
+    });
+})(jQuery);
